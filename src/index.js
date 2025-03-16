6fc2e864d69f986904af5ba970bc4ff361f29ccd
Bildad Ereggae <bildadereggae@gmail.com>
2025-03-16 23:26:43 +0300

    tasklister done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+
+  const submit = document.getElementById("submit");
+
+  let taskUl = document.getElementById("tasks");
+
+  let taskList = [];
+  
+  newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    taskList.push(taskInput);
+
+    const newTask = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x"
+    taskUl.style.position = "relative";
+
+    newTask.textContent = taskInput.value;
+
+    taskUl.appendChild(newTask);
+    taskInput.value = "";
+    newTask.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+      taskUl.removeChild(newTask)
+    })
+  })
 });
+
+
